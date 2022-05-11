1e83d8047649b6fe3780f790ba50caaef008bcda
Cesar Carvalho <cesaraugc@gmail.com>
2022-05-10 22:12:25 -0300

    Added filtering feature on recipes, tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -15,7 +15,16 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
     permission_classes = (IsAuthenticated,)
     
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by("-name")
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-name").distinct()
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -40,8 +49,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
     
+    def _params_to_ints(self, qs):
+        return [int(str_id) for str_id in qs.split(',')]
+    
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tags_ids)
+        if ingredients:
+            ingredients_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredients_ids)
+        
+        return queryset.filter(user=self.request.user).order_by("-id")
     
     def get_serializer_class(self):
         if self.action == 'retrieve':
